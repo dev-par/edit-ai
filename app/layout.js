a22e74bbaccc943bfc6411d42fa2b000671325a6
dev-par <146295367+dev-par@users.noreply.github.com>
2025-07-31 21:04:15 -0400

    Skeleton and basic background completed

---
+++ app/layout.js
@@ -0,0 +1,36 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
+import FloatingShapes from "@/components/floating-shapes";
+
+const inter = Inter({subsets: ["latin"],});
+
+export const metadata = {
+  title: "EditAI",
+  description: "AI image editor",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${inter.className}`}
+      >
+      <ThemeProvider 
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <main className="bg-slate-900 min-h-screen text-white overflow-x-hidden">
+          <FloatingShapes />
+          <Toaster richColors />
+          {children}
+        </main>
+        
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>Hello how are you <br/>
+    <Button variant="primary">Button</Button>
+    <Button variant="glass">Button</Button>
+    </div>
+  );
+}
